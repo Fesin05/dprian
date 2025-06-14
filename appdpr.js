80d9478d068a67c12c32c94c28aa31ab9efc8295
Fesin05 <felipesp1007@gmail.com>
2025-06-13 21:37:26 -0300

    Add files via upload

---
+++ appdpr.js
@@ -0,0 +1,12 @@
+function abrir() {
+    document.getElementById("galeria1").classList.remove("escondido");
+}
+function fechar() {
+    document.getElementById("galeria1").classList.add("escondido");
+}
+function abrir2() {
+    document.getElementById("galeria2").classList.remove("escondido2");
+}
+function fechar2() {
+    document.getElementById("galeria2").classList.add("escondido2");
+}
